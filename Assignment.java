7f7cf9ccae03e755105626bef67351ec703f24a0
vaibhav shinde <vaibhavshinde8265051460@gmail.com>
2026-09-24 14:29:14 +0530

    add assignment operator

---
+++ Assignment.java
@@ -0,0 +1,17 @@
+package Operator;
+
+public class Assignment {
+	public static void main(String[] args) {
+		//a=a+10
+		int a=10;
+		System.out.println(a+=2);
+		System.out.println(a-=2);
+		System.out.println(a*=2);
+		System.out.println(a/=2);
+		System.out.println(a%=2);
+		int b=10;
+		b+=10;
+		System.out.println(b);
+	}
+
+}
