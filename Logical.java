8b9eed98cf7eb90e8c4f920c8bc60638f935ffca
vaibhav shinde <vaibhavshinde8265051460@gmail.com>
2026-09-25 16:02:04 +0530

    add logical operator

---
+++ Logical.java
@@ -0,0 +1,12 @@
+package Operator;
+
+public class Logical {
+	public static void main(String[] args) {
+		//Logical AND
+		System.out.println((3<5) && (4<5));
+		//Logical OR
+		System.out.println((3<5) || (4>5));
+		//Logical NOT
+		System.out.println(!(3<5));
+	}
+}
